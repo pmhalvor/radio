9a2c20ce2e753e3ee807ed84007bda6ce007997b
Per Morten  Halvorsen <pmchalvorsen@gmail.com>
2025-03-09 22:50:08 +0100

    add user as local var

--- scripts/run-history-service.sh
+++ scripts/run-history-service.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-root=/home/pmhalvor
+user=pmhalvor
+
+root=/home/${user}
 tokenpath=/radio/.token
 datapath=/data
-docker pull  ghcr.io/pmhalvor/radio_history:latest
-docker run --rm --env-file ${root}/radio/.env -v ${root}${tokenpath}:${tokenpath} -v ${root}${datapath}:${datapath}  ghcr.io/pmhalvor/radio_history:latest
+
+docker pull  ghcr.io/${user}/radio_history:latest
+
+docker run --rm --env-file ${root}/radio/.env -v ${root}${tokenpath}:${tokenpath} -v ${root}${datapath}:${datapath}  ghcr.io/${user}/radio_history:latest
