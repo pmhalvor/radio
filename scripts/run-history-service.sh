2b0fa7994dc053fc6437f64678e999a8439983c4
pmhalvor <pmchalvorsen@gmail.com>
2025-03-09 22:42:49 +0100

    read env from absolute path

--- scripts/run-history-service.sh
+++ scripts/run-history-service.sh
@@ -4,4 +4,4 @@ root=/home/pmhalvor
 tokenpath=/radio/.token
 datapath=/data
 docker pull  ghcr.io/pmhalvor/radio_history:latest
-docker run --rm --env-file .env -v ${root}${tokenpath}:${tokenpath} -v ${root}${datapath}:${datapath}  ghcr.io/pmhalvor/radio_history:latest
+docker run --rm --env-file ${root}/radio/.env -v ${root}${tokenpath}:${tokenpath} -v ${root}${datapath}:${datapath}  ghcr.io/pmhalvor/radio_history:latest
