767fa5f634d9b26d8d558ae4221b1f797fd4de10
Per Halvorsen <31341520+pmhalvor@users.noreply.github.com>
2025-03-09 15:36:23 +0100

    Initial setup (#1)
    * fix redirects and authorization flows
    
    * rm unsed route + add doc str to callback methods
    
    * add rest of files from pmhalvor/website
    
    * no playing album
    
    * save full token info on first and refresh
    
    * move styling to test background img load (failed)
    
    * move static
    
    * remove breakpoint
    
    * use spotify methods (get current and recents)
    
    * clean up current + plot recents w/ df + show recent albums
    
    * add make commands for docker
    
    * add docker compose flask (manual volume config required)
    
    * add GHA workflow
    
    * add streamlit dockerfile
    
    * print current directory in streamlit app
    
    * add logging of current dir
    
    * log local/static directory
    
    * remove wildcard from COPY
    
    * hide printing in flask app
    
    * connect flask and streamlit service
    
    * force flask to use host 0.0.0.0
    
    * change default and use trusted environ.get method
    
    * add ex nginx file for guidance on how to safely serve streamlit as external app
    
    * add album hrefs to track url
    
    * add wait on data load + color plot dark blue
    
    * add st run make target
    
    * st specific styling to better match site design
    
    * basic readme for getting started
    
    * give horizontal block more space
    
    * fixed margins btwn st blocks
    
    * add iframe resizer + fix stApp background
    
    * test using env vars in GHA
    
    * build tag from branch and commit
    
    * add tagging to docker compose
    
    * test tag export
    
    * run test on tags
    
    * use github context to get tag ref name
    
    * rm refs/head from tag
    
    * ignore tags on branch deploy
    
    * update name on tag deploy wf
    
    * ignore tags when dpeloying from master

---
+++ Makefile
@@ -0,0 +1,16 @@
+run:
+	python app.py & streamlit run app_streamlit.py
+
+run-streamlit:
+	export API_HOST=0.0.0.0 && streamlit run app_streamlit.py
+
+run-flask:
+	python app.py
+
+docker-flask:
+	docker build -t radio_flask:alpha -f Dockerfile.flask .
+	docker run radio_flask:alpha
+
+docker-flask-stop:
+	docker ps | grep radio_flask | xargs docker stop
+	# docker stop $(docker ps -a -q --filter ancestor=radio_flask:alpha)
--- src/authorize.py
+++ src/authorize.py
@@ -1,12 +1,63 @@
-import base64, logging, io, json, os, requests, six, time
+import base64
+import json
+import os
+import requests
+import six
+import time
+import urllib.parse
+import webbrowser
+
 from dotenv import load_dotenv
 
 # Load environment variables
 load_dotenv()
 
-DATAPATH = '.data'
+CACHE_CODE_PATH = '.cache_code'
+TOKEN_FILE = '.token'
+PORT = 8888
+RETRY = 10
 
-# Authorization for Spotify
+### MAIN AUTHORIZATION FUNCTIONS
+def get_code() -> str:
+    """
+    Triggers authiorization flow and returns code from callback.
+    The code can be used in get_token_first_time() to get access token.
+    """
+    # parameters for post request
+    OAUTH_TOKEN_URL = "https://accounts.spotify.com/authorize"
+    PAYLOAD = {
+        'client_id': os.environ.get('SPOTIFY_CLIENT_ID'),
+        'scope': 'user-read-currently-playing user-read-recently-played',
+        'redirect_uri': f'http://localhost:{PORT}/callback/authorized',  # define in Spotify Developer Dashboard
+        'response_type': 'code'
+    }
+    authorization_url = f"{OAUTH_TOKEN_URL}?{urllib.parse.urlencode(PAYLOAD)}"
+
+    # open browser to get authorization code
+    webbrowser.open(authorization_url, 2)  
+
+    code = None 
+    counter = 0
+    while code is None:
+        # handle two different connection types
+        if os.path.exists(CACHE_CODE_PATH):
+            # inside flask server
+            with open(CACHE_CODE_PATH, 'r') as f:
+                code = f.read()
+        else:
+            # inside streamlit server
+            code = requests.get(f'http://localhost:{PORT}/callback/code').json().get('code', None)
+        
+        time.sleep(1)
+        counter += 1
+        if counter > RETRY: 
+            break
+        
+    requests.get(f'http://localhost:{PORT}/cache/clear')  # clean up credentials from server
+    
+    return code
+
+
 def get_token() -> str:
     '''
     Steps:
@@ -25,31 +76,79 @@ def get_token() -> str:
     return cache_token['token']
 
 
-# Read cached token from FileShare
 def get_cache_token() -> dict:
     """
-    Steps:
-        X load local data
+    Tries to load local data from TOKEN_FILE.
+    If file is not found or invalid, it will try to get a new token.
         X return payload
     """
 
-    path = DATAPATH
     try:
-        with open(path, 'r') as f:
+        with open(TOKEN_FILE, 'r') as f:
             data = json.load(f)
     except (FileNotFoundError, json.JSONDecodeError):
-        data = {}
+        data = get_token_first_time()
 
-    payload = {
-        "refresh_token": data.get("refresh_token", None),
-        "timestamp": data.get("timestamp", None),
-        "token": data.get("token", None),
+    return data
+    # payload = {
+    #     "refresh_token": data.get("refresh_token", None),
+    #     "timestamp": data.get("timestamp", None),
+    #     "token": data.get("token", None),
+    #     "scope": data.get("scope", None),
+    #     "expires_in": data.get("expires_in", None),
+    #     "token_type": data.get("token_type", None),
+    #     "access_token": data.get("access_token", None),
+    # }
+
+    # return payload
+
+
+def get_token_first_time(code=None) -> dict:
+    '''
+    Steps:
+        X set correct payload (refresh_token)
+        X set correct header  (client id and secret)
+        X post request
+        X convert response to json
+        X update token_info in FileShare cache
+        X return token info
+    '''
+    if not code:
+        code = get_code()
+    
+    # parameters for post request
+    OAUTH_TOKEN_URL = "https://accounts.spotify.com/api/token"
+    PAYLOAD = {
+        'grant_type': 'authorization_code',
+        'redirect_uri': f'http://localhost:{PORT}/callback/authorized',  # defined in Spotify Developer Dashboard
+        'code': code,
     }
+    HEADERS = make_headers()
 
-    return payload
+    # post request
+    response = requests.post(
+        url=OAUTH_TOKEN_URL,
+        data=PAYLOAD,
+        headers=HEADERS
+    )
+    token_info = response.json()
+    # print(f"Returned token info: \n {token_info}")
+    
+    if "access_token" not in token_info:
+        print("No access token found")
+        return {}
+    else:
+        # add needed key-values to token info before storing
+        token_info["timestamp"] = int(time.time())
+        token_info["token"] = token_info["access_token"]
+
+    # store for later use
+    store_token(token_info)
+
+    return token_info
 
 
-# Check if cached token is still valid
+### HELPER FUNCTIONS
 def is_token_expired(token_info) -> bool:
     """
     Steps:
@@ -72,7 +171,6 @@ def is_token_expired(token_info) -> bool:
     return expired
 
 
-# Refresh token from cache
 def refresh_access_token(refresh_token) -> dict:
     '''
     Steps:
@@ -100,26 +198,28 @@ def refresh_access_token(refresh_token) -> dict:
         headers=HEADERS
     )
     token_info = response.json()
+    token_info["timestamp"] = int(time.time())
+    token_info["refresh_token"] = refresh_token
+    token_info["token"] = token_info["access_token"]
 
-    print(f"Returned token info: \n {token_info}")
-    # Handle cases where refresh token might be missing
-    returned_refresh_token = token_info.get("refresh_token", refresh_token)
-    if "access_token" not in token_info:
-        return {}
+    # print(f"Returned token info: \n {token_info}")
+    # # Handle cases where refresh token might be missing
+    # returned_refresh_token = token_info.get("refresh_token", refresh_token)
+    # if "access_token" not in token_info:
+    #     return {}
 
-    payload = {
-        "refresh_token": returned_refresh_token,
-        "token": token_info["access_token"],
-        "timestamp": int(time.time())
-    }
+    # # payload = {  # TODO check behavior on refresh token
+    # #     "refresh_token": returned_refresh_token,
+    # #     "token": token_info["access_token"],
+    # #     "timestamp": int(time.time())
+    # # }
 
     # store tokens for later
-    store_renewed_token(payload)
+    store_token(token_info)
 
-    return payload
+    return token_info
 
 
-# Make header for token request
 def make_headers() -> dict:
     client_id = os.environ.get('SPOTIFY_CLIENT_ID')         # Spotify cliient id stored as local env. var.
     client_secret = os.environ.get('SPOTIFY_CLIENT_SECRET') # Spotify client secret stored as local env. var.
@@ -131,71 +231,17 @@ def make_headers() -> dict:
     return {"Authorization": f"Basic {client.decode('ascii')}", "Content-Type": "application/x-www-form-urlencoded"}
 
 
-# Store the renewed token locally
-def store_renewed_token(token_info):
-    path = DATAPATH
-    with open(path, 'w') as f:
+def store_token(token_info) -> bool:
+    with open(TOKEN_FILE, 'w') as f:
         json.dump(token_info, f, indent=4)
+    # if os.environ.get("SPTOFIY_TOKEN") != token_info.get("token"):
+        # os.environ["SPOTIFY_TOKEN"] = token_info.get("token")
     return True
 
 
-# Get first token with access code
-def get_token_first_time(code) -> dict:
-    '''
-    Steps:
-        X set correct payload (refresh_token)
-        X set correct header  (client id and secret)
-        X post request
-        X convert response to json
-        X update token_info in FileShare cache
-        X return token info
-    '''
-    # parameters for post request
-    OAUTH_TOKEN_URL = "https://accounts.spotify.com/api/token"
-    PAYLOAD = {
-        'grant_type': 'authorization_code',
-        'redirect_uri': 'http://localhost:5001/callback',  # Flask callback URL
-        'code': code,
-    }
-    HEADERS = make_headers()
-
-    # post request
-    response = requests.post(
-        url=OAUTH_TOKEN_URL,
-        data=PAYLOAD,
-        headers=HEADERS
-    )
-    token_info = response.json()
-
-    # add needed key-values to token info before storing
-    token_info["timestamp"] = int(time.time())
-    token_info["token"] = token_info["access_token"]
-
-    # store for later use
-    store_renewed_token(token_info)
-
-    return token_info
-
-
-# Build url to generate access code (cannot use requests to call)
-def get_code_url():
-    """
-    Can not call requests to get access code.
-    """
-
-    # parameters for post request
-    OAUTH_TOKEN_URL = "https://accounts.spotify.com/authorize"
-    PAYLOAD = {
-        'client_id': os.environ.get('SPOTIFY_CLIENT_ID'),
-        'scope': 'user-read-currently-playing user-read-recently-played',
-        'redirect_uri': 'http://localhost:5001/callback', # Flask callback URL
-        'response_type': 'code'
-    }
-    import urllib.parse
-    return f"{OAUTH_TOKEN_URL}?{urllib.parse.urlencode(PAYLOAD)}"
-
 
 if __name__ == "__main__":
-    print(get_code_url())
-    print(get_cache_token())
+    ### TESTING (expects app_flask.py to be running locally on port 8888)
+
+    # print(get_code())
     print(get_token())
