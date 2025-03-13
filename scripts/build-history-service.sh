4ced2d5c341402baf1fdc62119be600f78d78232
Per Morten  Halvorsen <pmchalvorsen@gmail.com>
2025-03-13 12:42:12 +0100

    clean up scripts, add interactive build/push & local run

--- scripts/build-history-service.sh
+++ scripts/build-history-service.sh
@@ -4,4 +4,12 @@
 user=pmhalvor
 
 cd /home/${user}/radio/
-docker build -t ghcr.io/${user}/radio_history:latest -f Dockerfile.history .
+docker build -t ghcr.io/${user}/radio_history:latest -f Dockerfile.history .  --platform linux/amd64
+
+echo "Do you want to push the latest version to the container registry? (y/[n]): "
+read answer
+if [[ $answer == "y" ]]; then
+    docker push ghcr.io/${user}/radio_history:latest
+else
+    echo "Skipping push to container registry."
+fi
--- scripts/local-run-history-service.sh
+++ scripts/local-run-history-service.sh
@@ -7,6 +7,13 @@ root=/home/${user}
 tokenpath=/radio/.token
 datapath=/data
 
-docker pull  ghcr.io/${user}/radio_history:latest
+echo "Pull latest version from registry? (y/[n]): "
+read answer
+if [[ $answer == "y" ]]; then
+    docker pull  ghcr.io/${user}/radio_history:latest
+else
+    echo "Using local latest version."
+    echo $(docker images | grep ghcr.io/${user}/radio_history | awk '{print $3}')
+fi
 
 docker run --rm --env-file ${root}/radio/.env -v ${root}${tokenpath}:${tokenpath} -v ${root}${datapath}:${datapath}  ghcr.io/${user}/radio_history:latest
